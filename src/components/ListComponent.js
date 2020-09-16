c94d3c9985afe68530e8634c77126af7c9cafb0f
unknown <rajibkhan976@gmail.com>
2020-09-16 18:10:38 +0200

    list page completed

--- src/components/ListComponent.js
+++ src/components/ListComponent.js
@@ -5,10 +5,9 @@ import tableDnD from 'tablednd/dist/jquery.tablednd.js';
 const ListComponent = ({}) => {
 	
 	const [listHeader, setListHeader] = useState(null);
+	const [listData, setListData] = useState(null);
 	const [listRows, setListRows] = useState(null);
 	const [error, setError] = useState(null);
-	const [searchKeyword, setSearchKeyword] = useState(null);
-	const [searchResult, setSearchResult] = useState(null);
 	
 	useEffect(() => {
 		fetch(`http://localhost:8080/api/list.php`, {
@@ -28,34 +27,188 @@ const ListComponent = ({}) => {
 		});
 	}, []);
 	
-	const handleSearchChange = (event) => {
-		setSearchKeyword(event.target.value);
-	}
-	
 	useEffect(() => {
-		if (searchKeyword !== null && listRows !== null) {
-			let searchResult = [];
-			searchResult = listRows.filter((element, index) => {
-				if (searchKeyword == element.id ||
-				searchKeyword === element.name || 
-				searchKeyword === element.created_at
-				) {
-					return element;
-				}
-			});
-			setSearchResult(searchResult);
+		if (listRows !== null) {
+			setListData(listRows);
 		}
-	}, [searchKeyword]);
+	}, [listRows]);
 	
 	jQuery(document).ready(function() {
 		jQuery("#list-table").tableDnD();
 	});
 	
+	const [searchKeyword, setSearchKeyword] = useState(null);
+	
+	const handleSearchChange = (event) => {
+		setSearchKeyword(event.target.value);
+	}
+	
+	const [searchResult, setSearchResult] = useState(null);
+	
+	useEffect(() => {
+		if (searchKeyword !== null && listRows !== null) {
+			let result = [];
+			result = listRows.filter((element, index) => {
+				if (searchKeyword.trim() == element.id ||
+					searchKeyword.trim().toLowerCase() === element.name.toLowerCase() || 
+					searchKeyword.trim() === element.created_at
+				) {
+					return element;
+				}
+			});
+			setSearchResult(result);
+		}
+	}, [searchKeyword]);
+	
+	useEffect(() => {
+		if (searchResult !== null) {
+			setListData(searchResult);
+		}
+	}, [searchResult]);
+	
+	const [sortListBy, setSortListBy] = useState("");
+	
+	const sortList = (event) => {
+		setSortListBy(event.target.value);
+	}
+	
+	const [sortedList, setSortedList] = useState(null);
+	
+	useEffect(() => {
+		if (sortListBy !== ""  && listData !== null && listData.length !== 0) {
+			if (sortListBy.toLowerCase() === "id") {
+				let sortedById = [];
+				listData.sort(function (a, b) {
+						return b.id - a.id;
+					}).map((element) => {
+						sortedById.push(element);
+					});
+				setSortedList(sortedById);
+			} 
+			if (sortListBy.toLowerCase() === "name") {
+				let sortedByName = [];
+				listData.sort(function (a, b) {
+						var nameA = a.name.toUpperCase(); 
+						var nameB = b.name.toUpperCase();
+					
+						  if (nameA < nameB) {
+							return -1;
+						  }
+						  
+						  if (nameA > nameB) {
+							return 1;
+						  }
+						  
+						  return 0;
+					}).map((element) => {
+						sortedByName.push(element);
+					});
+				setSortedList(sortedByName);
+			} 
+			if (sortListBy.toLowerCase() === "submision date") {
+				let sortedByDate = [];
+				listData.sort(function (a, b) {
+						var createdA = a.created_at; 
+						var createdB = b.created_at; 
+					
+						  if (createdA < createdB) {
+							return -1;
+						  }
+						  
+						  if (createdA > createdB) {
+							return 1;
+						  }
+						  
+						  return 0;
+					}).map((element) => {
+						sortedByDate.push(element);
+					});
+				setSortedList(sortedByDate);
+			}
+		} else {
+			if (sortListBy !== ""  && listRows !== null) {
+				if (sortListBy.toLowerCase() === "id") {
+					let sortedById = [];
+					listRows.sort(function (a, b) {
+							return b.id - a.id;
+						}).map((element) => {
+							sortedById.push(element);
+						});
+					setSortedList(sortedById);
+				} 
+				if (sortListBy.toLowerCase() === "name") {
+					let sortedByName = [];
+					listRows.sort(function (a, b) {
+							var nameA = a.name.toUpperCase(); 
+							var nameB = b.name.toUpperCase();
+						
+							  if (nameA < nameB) {
+								return -1;
+							  }
+							  
+							  if (nameA > nameB) {
+								return 1;
+							  }
+							  
+							  return 0;
+						}).map((element) => {
+							sortedByName.push(element);
+						});
+					setSortedList(sortedByName);
+				} 
+				if (sortListBy.toLowerCase() === "submision date") {
+					let sortedByDate = [];
+					listRows.sort(function (a, b) {
+							var createdA = a.created_at; 
+							var createdB = b.created_at; 
+						
+							  if (createdA < createdB) {
+								return -1;
+							  }
+							  
+							  if (createdA > createdB) {
+								return 1;
+							  }
+							  
+							  return 0;
+						}).map((element) => {
+							sortedByDate.push(element);
+						});
+					setSortedList(sortedByDate);
+				}
+			}
+		}
+	}, [sortListBy]);
+	
+	useEffect(() => {
+		if (sortedList !== null) {
+			setListData(sortedList);
+		}
+	}, [sortedList]);
+	
 	return (
 		<div className="container">
 			<div className="row mt-4 mb-4">
-				<div className="col-9">
+				<div className="col-6">
 					<h1 className="float-left">User list</h1>
+				</div>
+				<div className="col-3 mt-2">
+					<div className="input-group">
+					  <select className="custom-select" value={sortListBy} onChange={(event) => sortList(event)}>
+						{listHeader ? 
+						listHeader.map((element, index) => {
+							return <optgroup key={index} label="sort by">
+									  <option>{""}</option>
+									  <option>{element.id.title}</option>
+									  <option>{element.name.title}</option>
+									  <option>{element.created_at.title}</option>
+							</optgroup>
+						})
+						:
+						null
+						}
+					  </select>
+					</div>
 				</div>
 				<div className="col-3 mt-2">
 					<div className="input-group">
@@ -87,20 +240,20 @@ const ListComponent = ({}) => {
 						}
 						</thead>
 						<tbody>
-						{(searchResult !== null && searchResult.length !== 0) ?
-						searchResult.map((element, index) => {
+						{(listData !== null && listData.length !== 0) ?
+						listData.map((element, index) => {
 							return <tr key={index}>
-							  <th scope="row">{element.id}</th>
+							  <td>{element.id}</td>
 							  <td>{element.name}</td>
 							  <td>{element.message}</td>
 							  <td>{element.created_at}</td>
 							</tr>
 						})
 						:
-						(listRows) ?
+						(listRows !== null && listRows.length !== 0) ?
 						listRows.map((element, index) => {
 							return <tr key={index}>
-							  <th scope="row">{element.id}</th>
+							  <td>{element.id}</td>
 							  <td>{element.name}</td>
 							  <td>{element.message}</td>
 							  <td>{element.created_at}</td>
